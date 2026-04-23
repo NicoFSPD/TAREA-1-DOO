fb27bb5d56d2524647d01c064fb42e52a255c131
Daniel Cristobal Patricio Lopez Ramirez <lopezramirezdaniel946@gmail.com>
2026-04-23 19:42:13 -0400

    Implementacion de interfaz Comparable, funcion compareTo y toString en clase Moneda

--- src/main/java/org/example/Moneda.java
+++ src/main/java/org/example/Moneda.java
@@ -1,23 +1,33 @@
 package org.example;
 
-public abstract class Moneda {
-    public Moneda(){
+public abstract class Moneda implements Comparable<Moneda> {
+    public Moneda() {
     }
 
-    public Moneda getSerie(){
+    public Moneda getSerie() {
         return this;
     }
 
     public abstract int getValor();
+
+    @Override
+    public int compareTo(Moneda otraMoneda) {
+        return Integer.compare(this.getValor(), otraMoneda.getValor());
+    }
+
+    @Override
+    public String toString() {
+        return "Moneda de " + this.getValor() + " (Serie: " + this.hashCode() + ")";
+    }
 }
 
-class Moneda1500 extends Moneda{
-    public Moneda1500(){
+class Moneda1500 extends Moneda {
+    public Moneda1500() {
         super();
     }
 
     @Override
-    public int getValor(){
+    public int getValor() {
         return 1500;
     }
 }
@@ -28,29 +38,29 @@ class Moneda1000 extends Moneda {
     }
 
     @Override
-    public int getValor(){
+    public int getValor() {
         return 1000;
     }
 }
 
-class Moneda500 extends Moneda{
-    public Moneda500(){
+class Moneda500 extends Moneda {
+    public Moneda500() {
         super();
     }
 
     @Override
-    public int getValor(){
+    public int getValor() {
         return 500;
     }
 }
 
-class Moneda100 extends Moneda{
-    public Moneda100(){
+class Moneda100 extends Moneda {
+    public Moneda100() {
         super();
     }
 
     @Override
-    public int getValor(){
+    public int getValor() {
         return 100;
     }
 }
