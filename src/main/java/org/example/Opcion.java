6dcd0501607b7fedb9e9077b57199decbd1437a4
NicoFSPD <linegarcina@gmail.com>
2026-04-23 22:09:16 -0400

    docs: Actualizacion en documentacion de la enumeracion Opcion

--- src/main/java/org/example/Opcion.java
+++ src/main/java/org/example/Opcion.java
@@ -1,23 +1,34 @@
 package org.example;
 
+/**
+ * Una enumeracion que le entrega a los productos constantes que los diferencian
+ * @author Nicolas Silva
+ * @version version 1, 23 de abril de 2026
+ */
 enum Opcion {
+/** Se definen desde aqui, las constantes*/
     COCA(1,1500),
     SPRITE(2, 1500),
     FANTA(3, 1500),
     SNICKERS(4, 1300),
     SUPER8(5, 1000);
-
+/** Dos valores int por producto: Su identificador (ID) y su precio*/
     private final int ID;
     private final int precio;
-
+/** El constructor le asignara a las variables, las constantes predefinidas*/
     Opcion(int ID, int precio){
         this.ID = ID;
         this.precio = precio;
     }
-
+/** getter para el Identificador
+ * @return Identificador "ID"
+ */
     public int getID() {
         return ID;
     }
+/** getter para el precio
+ * @return precio "precio"
+ */
     public int getPrecio(){
         return precio;
     }
