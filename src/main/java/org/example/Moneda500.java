f6c70c892c3c80da8e08a408469bc91b93047292
NicoFSPD <linegarcina@gmail.com>
2026-05-01 11:01:52 -0400

    fix: ordenar conflictos por archivos duplicados

--- src/main/java/org/example/Moneda500.java
+++ src/main/java/org/example/Moneda500.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public class Moneda500 {
+class Moneda500 extends Moneda {
+    public Moneda500() {
+        super();
+    }
+
+    @Override
+    public int getValor() {
+        return 500;
+    }
 }
